237251fd9c708750a8a5e59ba9744e4b7e77a6ad
Chryptony <chryptony@gmail.com>
2023-12-15 16:44:16 +0700

    percobaan 1

---
+++ basic-react/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>belajar react</h1>
+      <p>bla bla bla</p>
+    </div>
+  );
+}
+
+export default App;
