b1b0f1b03a2f70de075a5f9bfd62ace41089ffe3
Chryptony <chryptony@gmail.com>
2023-12-16 13:11:50 +0700

    latihan

--- basic-react/src/App.js
+++ basic-react/src/App.js
@@ -1,13 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import images from './images/logo512.png'
+// kalau gambarnya ada 10 biji berarti import imagenya ada 10 kali copy paste
+// kata image di kata setelah import adalah variabel . tanda " . " titik di "./images/logo512.png" mewakili src bukan basic react
 function App() {
   return (
     <div className="App">
       <h1>belajar react</h1>
       <p>bla bla bla</p>
+      <img src={images}/>
     </div>
   );
 }
 
 export default App;
+// biasanya pakai buat view homepage
